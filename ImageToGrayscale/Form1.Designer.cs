00faf6b7f7d57b69d9571b8c47a31112f4a05091
ChocolateFush <michaeltrsadlier@gmail.com>
2016-09-26 23:17:18 +1300

    Initial release test

---
+++ ImageToGrayscale/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ImageToGrayscale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_button = new System.Windows.Forms.Button();
+            this.picture_box = new System.Windows.Forms.PictureBox();
+            this.grayscale_button = new System.Windows.Forms.Button();
+            this.g_blur_button = new System.Windows.Forms.Button();
+            this.edge_detect = new System.Windows.Forms.Button();
+            this.sensitivity = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensitivity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open_button
+            // 
+            this.open_button.Location = new System.Drawing.Point(13, 13);
+            this.open_button.Name = "open_button";
+            this.open_button.Size = new System.Drawing.Size(75, 23);
+            this.open_button.TabIndex = 0;
+            this.open_button.Text = "Open";
+            this.open_button.UseVisualStyleBackColor = true;
+            this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // picture_box
+            // 
+            this.picture_box.Location = new System.Drawing.Point(1, 86);
+            this.picture_box.Name = "picture_box";
+            this.picture_box.Size = new System.Drawing.Size(259, 175);
+            this.picture_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picture_box.TabIndex = 1;
+            this.picture_box.TabStop = false;
+            // 
+            // grayscale_button
+            // 
+            this.grayscale_button.Location = new System.Drawing.Point(107, 12);
+            this.grayscale_button.Name = "grayscale_button";
+            this.grayscale_button.Size = new System.Drawing.Size(75, 23);
+            this.grayscale_button.TabIndex = 2;
+            this.grayscale_button.Text = "Grayscale";
+            this.grayscale_button.UseVisualStyleBackColor = true;
+            this.grayscale_button.Click += new System.EventHandler(this.grayscale_button_Click);
+            // 
+            // g_blur_button
+            // 
+            this.g_blur_button.Location = new System.Drawing.Point(205, 13);
+            this.g_blur_button.Name = "g_blur_button";
+            this.g_blur_button.Size = new System.Drawing.Size(75, 38);
+            this.g_blur_button.TabIndex = 3;
+            this.g_blur_button.Text = "Gaussian Blur";
+            this.g_blur_button.UseVisualStyleBackColor = true;
+            this.g_blur_button.Click += new System.EventHandler(this.g_blur_button_Click);
+            // 
+            // edge_detect
+            // 
+            this.edge_detect.Location = new System.Drawing.Point(314, 13);
+            this.edge_detect.Name = "edge_detect";
+            this.edge_detect.Size = new System.Drawing.Size(75, 23);
+            this.edge_detect.TabIndex = 4;
+            this.edge_detect.Text = "Edge Detect";
+            this.edge_detect.UseVisualStyleBackColor = true;
+            this.edge_detect.Click += new System.EventHandler(this.edge_detect_Click);
+            // 
+            // sensitivity
+            // 
+            this.sensitivity.Location = new System.Drawing.Point(314, 55);
+            this.sensitivity.Name = "sensitivity";
+            this.sensitivity.Size = new System.Drawing.Size(104, 45);
+            this.sensitivity.TabIndex = 5;
+            this.sensitivity.Value = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(445, 265);
+            this.Controls.Add(this.sensitivity);
+            this.Controls.Add(this.edge_detect);
+            this.Controls.Add(this.g_blur_button);
+            this.Controls.Add(this.grayscale_button);
+            this.Controls.Add(this.picture_box);
+            this.Controls.Add(this.open_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture_box)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sensitivity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open_button;
+        private System.Windows.Forms.PictureBox picture_box;
+        private System.Windows.Forms.Button grayscale_button;
+        private System.Windows.Forms.Button g_blur_button;
+        private System.Windows.Forms.Button edge_detect;
+        private System.Windows.Forms.TrackBar sensitivity;
+    }
+}
+
---
+++ ImageToGrayscale/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToGrayscale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap image_import;
+
+        private void open_button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog open_dlg = new OpenFileDialog())
+            {
+                open_dlg.Title = "Open Image";
+                open_dlg.Filter = "bmp files(*.bmp)|*.bmp";
+
+                if (open_dlg.ShowDialog() == DialogResult.OK)
+                {
+                    image_import = new Bitmap(open_dlg.FileName);
+                    picture_box.Image = image_import;
+                }
+            }
+            
+        }
+
+        private void grayscale_button_Click(object sender, EventArgs e)
+        {
+            Bitmap d = new Bitmap(image_import.Width, image_import.Height);
+
+            for (int i = 0; i < image_import.Width; i++) {
+                for (int j = 0; j < image_import.Height; j++) {
+
+                    Color oc = image_import.GetPixel(i, j);
+                    int grayscale = (int)((oc.R * 0.3) + (oc.G * 0.59) + (oc.B * 0.11));
+                    Color nc = Color.FromArgb(oc.A, grayscale, grayscale, grayscale);
+                    d.SetPixel(i, j, nc);
+                }
+
+            }
+
+            image_import = d;
+            picture_box.Image = image_import;
+            
+        }
+
+        private void g_blur_button_Click(object sender, EventArgs e)
+        {
+            int[,] kernel = new int[3, 3] { {1,2,1}, {2,4,2}, {1,2,1} };
+
+            Bitmap r = new Bitmap(image_import.Width, image_import.Height);
+            int sum = 0;
+            int current_value = 0;
+
+
+            for (int i = 2; i < image_import.Width - 2; i++) {
+                for (int j = 2; j < image_import.Height - 2; j++) {
+                    for (int k = 0; k < 3; k++) {
+                        for (int l = 0; l < 3; l++) {
+                            current_value = image_import.GetPixel(i + k, j + l).R;
+                            sum += current_value * kernel[k,l];
+                        }
+                    }
+                    sum = sum / 16;
+                    r.SetPixel(i+1, j+1, Color.FromArgb(sum,sum,sum));
+                    sum = 0;
+                }
+            }
+            image_import = r;
+            picture_box.Image = image_import;
+        }
+
+        private void edge_detect_Click(object sender, EventArgs e)
+        {
+            int[,] hori_kernel = new int[3, 3] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] verti_kernel = new int[3, 3] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+            Bitmap r = new Bitmap(image_import.Width, image_import.Height);
+            Bitmap s = new Bitmap(image_import.Width, image_import.Height);
+            int current_value = 0;
+            int sum_product_h = 0;
+            int sum_product_v = 0;
+            int sum_total = 0;
+
+            for (int i = 2; i < image_import.Width - 2; i++)
+            {
+                for (int j = 2; j < image_import.Height - 2; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            current_value = image_import.GetPixel(i + k, j + l).R;
+                            sum_product_h += current_value * hori_kernel[k, l];
+                            sum_product_v += current_value * verti_kernel[k, l];
+                        }
+                    }
+                    sum_total = (int)Math.Sqrt(Math.Pow(sum_product_h, 2) + Math.Pow(sum_product_v, 2))/(16 + (10-sensitivity.Value));
+                    r.SetPixel(i, j, Color.FromArgb(sum_total, sum_total, sum_total));
+                    sum_product_h = 0;
+                    sum_product_v = 0;
+                }
+            }
+
+            image_import = r;
+            picture_box.Image = image_import;
+
+            for (int i = 0; i < image_import.Width; i++) {
+                for (int j = 0; j < image_import.Height; j++) {
+                    if (image_import.GetPixel(i, j).R > 5)
+                    {
+                        r.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+                    else {
+                        r.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+
+                }
+            }
+
+            image_import = r;
+            picture_box.Image = image_import;
+
+        }
+    }
+}
